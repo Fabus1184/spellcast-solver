4bd058c1c1f8e6b5d6a8e2cd400c0c31f0c0f3fd
Fabian Lippold <fabianlippold1184@gmail.com>
2023-02-19 17:12:34 +0100

    update

--- app/Main.hs
+++ app/Main.hs
@@ -1,31 +1,22 @@
-{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TupleSections #-}
 
 module Main (main) where
 
-import Control.Concurrent (modifyMVar_, newMVar, readMVar)
-import Control.Concurrent.Async (mapConcurrently_)
-import Control.Lens (imap)
 import Criterion.Measurement (getTime, initializeTime)
-import Data.Array (Array, array, inRange, (!))
-import Data.Char (toLower, toUpper)
-import Data.Foldable (Foldable (toList))
-import Data.List.Extra (chunksOf, nubOrd, sortOn, (\\))
-import Data.Maybe (fromMaybe)
-import Data.Tuple.Extra (both, fst3)
-import Formatting (formatToString, int, right, string, (%), (%.))
+import Data.Char (toUpper)
+import Data.Ix (inRange)
+import Data.List.Extra (chunksOf, sortOn)
+import Data.Tuple.Extra (both, thd3)
+import Formatting (fixed, formatToString, int, right, shown, string, (%), (%.))
 
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.HashMap.Strict as HM
-import qualified Data.HashSet as HS
 import qualified Data.Trie as T
 
-type Grid = Array Int Char
-
 -- | Print a 5x5 grid
-printGrid :: Grid -> IO ()
-printGrid arr = mapM_ (putStrLn . unwords . map (pure . toUpper)) . chunksOf 5 $ toList arr
+printGrid :: [Char] -> IO ()
+printGrid = mapM_ (putStrLn . unwords . map (pure . toUpper)) . chunksOf 5
 
 -- | Get the neighbours of a given row and col in a 5x5 grid
 neighbours' :: (Int, Int) -> [(Int, Int)]
@@ -46,80 +37,93 @@ neighbours' (row, col) =
 neighbours :: Int -> [Int]
 neighbours i = map (\(r, c) -> r * 5 + c) $ neighbours' (i `div` 5, i `mod` 5)
 
--- | Depth-first search of a 5x5 grid with a given maximum depth
-dfs :: Int -> Int -> [[Int]]
-dfs maxDepth start =
-    dfs' maxDepth start []
-  where
-    dfs' :: Int -> Int -> [Int] -> [[Int]]
-    dfs' 0 _ vt = [vt]
-    dfs' depth start' vt =
-        let ns = neighbours start' \\ vt
-         in nubOrd $ concatMap (\n -> dfs' (depth - 1) n (start' : vt)) ns
-
 -- | The points for each letter
 points :: HM.HashMap Char Int
 points =
     HM.fromList
-        [ ('a', 1)
-        , ('d', 3)
-        , ('e', 1)
-        , ('f', 5)
-        , ('g', 3)
-        , ('h', 4)
-        , ('i', 1)
-        , ('k', 6)
-        , ('l', 3)
-        , ('m', 4)
-        , ('n', 2)
-        , ('o', 1)
-        , ('p', 4)
-        , ('r', 2)
-        , ('s', 2)
-        , ('t', 2)
-        , ('u', 4)
-        , ('v', 5)
-        , ('w', 5)
-        , ('x', 7)
-        , ('y', 4)
-        , ('z', 8)
+        [ ('A', 1)
+        , ('B', 4)
+        , ('C', 5)
+        , ('D', 3)
+        , ('E', 1)
+        , ('F', 5)
+        , ('G', 3)
+        , ('H', 4)
+        , ('I', 1)
+        , ('J', 7)
+        , ('K', 6)
+        , ('L', 3)
+        , ('M', 4)
+        , ('N', 2)
+        , ('O', 1)
+        , ('Q', 8)
+        , ('P', 4)
+        , ('R', 2)
+        , ('S', 2)
+        , ('T', 2)
+        , ('U', 4)
+        , ('V', 5)
+        , ('W', 5)
+        , ('X', 7)
+        , ('Y', 4)
+        , ('Z', 8)
         ]
+
+reward :: BS.ByteString -> Int
+reward = sum . map (points HM.!) . BS.unpack
+
+type GridByIndex = HM.HashMap Int Char
+type GridByChar = HM.HashMap Char [Int]
+
+f :: [Int] -> GridByIndex -> [Char]
+f path gridByIndex = map (gridByIndex HM.!) path
+
+mapWords :: [Int] -> T.Trie () -> (GridByIndex, GridByChar) -> T.Trie [Int]
+mapWords [] trie (gridByIndex, gridByChar) =
+    foldl1 T.unionL
+        . map (\i -> mapWords [i] trie (gridByIndex, gridByChar))
+        $ [0 .. 24]
+mapWords path trie (gridByIndex, gridByChar) =
+    let xs = filter (`notElem` path) (neighbours (last path))
+        xs' = map (\x -> path ++ [x]) xs
+        xs'' = map (\x -> (x, T.submap (BS.pack $ map (gridByIndex HM.!) x) trie)) xs'
+        xs''' = map fst $ filter (not . T.null . snd) xs''
+     in foldl T.unionL T.empty
+            . (:) (T.fromList $ filter ((`T.member` trie) . fst) [(BS.pack $ f path gridByIndex, path)])
+            . map (\x -> mapWords x (T.submap (BS.pack $ map (gridByIndex HM.!) x) trie) (gridByIndex, gridByChar))
+            $ xs'''
 
 main :: IO ()
 main = do
     initializeTime
-    putStrLn "HIER"
-    trie <- T.fromList . map (,()) . BS.lines <$> BS.readFile "words.txt"
-    let arr =
-            array (0, 24)
-                . imap (,)
-                . map toLower
-                . concat
-                $ [ "TSSOK"
-                  , "NODOV"
-                  , "AMPEN"
-                  , "STXEG"
-                  , "ZWTYI"
-                  ]
-    results <- newMVar HS.empty
-    let ws i =
-            let !rs =
-                    HS.fromList
-                        . concatMap
-                            ( (map fst3 . T.matches trie)
-                                . BS.pack
-                                . map (arr !)
-                            )
-                        $ dfs 10 i
-             in modifyMVar_ results (pure . HS.union rs)
-    t <- getTime
-    mapConcurrently_ ws [0 .. 24]
-    readMVar results
-        >>= mapM_ (\(p, w) -> putStrLn $ formatToString ((right 12 ' ' %. string) % " " % int) w p)
-            . sortOn fst
-            . map (sum . fmap (\x -> fromMaybe (error (show x)) (HM.lookup x points)) >>= (,))
-            . fmap BS.unpack
-            . HS.toList
-    t' <- getTime
-    putStrLn $ "Took " ++ show (t' - t) ++ " seconds"
-    printGrid arr
+    t1 <- getTime
+
+    trie <- T.fromList . map (,()) . BS.lines . BS.map toUpper <$> BS.readFile "words.txt"
+    let grid =
+            concat
+                [ "MRSAJ"
+                , "ZOTOW"
+                , "NBOXV"
+                , "HNOQN"
+                , "IAITN"
+                ]
+    let gridByIndex = HM.fromList $ zip [0 ..] grid :: HM.HashMap Int Char
+    let gridByChar = HM.fromList $ map (\c -> (c, filter (\i -> HM.lookup i gridByIndex == Just c) [0 .. 24])) ['A' .. 'Z']
+    mapM_
+        ( \(w, p, r) ->
+            putStrLn $
+                formatToString
+                    ((right 12 ' ' %. string) % (right 5 ' ' %. int) % shown)
+                    (BS.unpack w)
+                    r
+                    p
+        )
+        . sortOn thd3
+        . map (\(w, p) -> (w, p, reward w))
+        . T.toList
+        $ mapWords [] trie (gridByIndex, gridByChar)
+
+    printGrid grid
+
+    t2 <- getTime
+    putStrLn $ formatToString ("took " % fixed 3 % "s") (t2 - t1)
